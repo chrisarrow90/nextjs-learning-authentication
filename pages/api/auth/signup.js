1185ab542c228963d3dd857b7e68f00e1a191cca
Chris Arrowsmith <chrisarrowsmith@gmail.com>
2021-10-28 15:20:30 +1300

    connect form to api

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,24 +1,35 @@
 import { connectToDatabase } from '../../../lib/db'
 import { hashPassword } from '../../../lib/auth'
 
-function handler(req, res) {
-  const { email, password } = req.body.data
+async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { email, password } = req.body
 
-  if (!email || !email.includes('@') || !password || password.trim().length < 7) {
-    res.status(422).json({ message: 'Invalid input' })
+    if (!email || !email.includes('@') || !password || password.trim().length < 7) {
+      res.status(422).json({ message: 'Invalid email or password' })
+    }
+
+    const client = await connectToDatabase()
+    const db = client.db()
+
+    const existingUser = await db.collection('users').findOne({ email: email })
+
+    if (existingUser) {
+      res.status(422).json({ message: 'User with that email address already exists' })
+      client.close()
+      return
+    }
+
+    const hashedPassword = await hashPassword(password)
+
+    await db.collection('users').insertOne({
+      email,
+      password: hashedPassword
+    })
+
+    res.status(201).json({ message: 'Created user' })
+    client.close()
   }
-
-  const client = await connectToDatabase()
-  const db = client.db()
-
-  const hashedPassword = hashPassword(password)
-
-  const result = await db.collection('users').insertOne({
-    email,
-    password: hashedPassword
-  })
-
-  res.status(201).json({ message: 'Created user' })
 }
 
 export default handler
